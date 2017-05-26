03da66645521f3d5e386759eace3ffc23c9924c0
KrGil <Gil@Changui-MacBook-Air.local>
2017-05-26 17:01:12 +0900

    Initial Commit

---
+++ Gilstalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gilstalk
+//
+//  Created by Chang ByoungGil on 2017. 5. 26..
+//  Copyright © 2017년 Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
